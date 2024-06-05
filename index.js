5400b54cfff858117cef47b01b8d67fa4e45bd92
Mamun-Ashraf <ashrafdkia@gmail.com>
2024-06-05 07:20:48 +0600

    updated product delete & patch API

--- index.js
+++ index.js
@@ -1,38 +1,38 @@
 require("dotenv").config();
 const express = require('express');
 const app = express()
-const jwt = require("jsonwebtoken");
+// const jwt = require("jsonwebtoken");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const port = 5000;
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const port = process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
 
-function createToken(user) {
-    const token = jwt.sign(
-        {
-            email: user.email,
-        },
-        "secret",
-        { expiresIn: "7d" }
-    );
-    return token;
-}
+// function createToken(user) {
+//     const token = jwt.sign(
+//         {
+//             email: user.email,
+//         },
+//         "secret",
+//         { expiresIn: "1y" }
+//     );
+//     return token;
+// }
 
 
-function verifyToken(req, res, next) {
-    const token = req.headers.authorization.split(" ")[1];
-    if (!token) {
-        return res.send("You are not authorized");
-    }
-    const verify = jwt.verify(token, "secret");
-    if (!verify?.email) {
-        return res.send("You are not authorized");
-    }
-    req.user = verify.email;
-    next();
-}
+// function verifyToken(req, res, next) {
+//     const token = req.headers.authorization.split(" ")[1];
+//     if (!token) {
+//         return res.send("You are not authorized");
+//     }
+//     const verify = jwt.verify(token, "secret");
+//     if (!verify?.email) {
+//         return res.send("You are not authorized");
+//     }
+//     req.user = verify.email;
+//     next();
+// }
 
 const uri = process.env.DATABASE_URL;
 
@@ -50,6 +50,7 @@ async function run() {
 
         await client.connect();
         const userCollection = client.db("best-shopping").collection("userCollection");
+        const productCollection = client.db("best-shopping").collection("productCollection");
 
         // user
 
@@ -68,7 +69,7 @@ async function run() {
         app.post("/user", async (req, res) => {
             const user = req.body;
 
-            const token = createToken(user);
+            // const token = createToken(user);
             const isUserExist = await userCollection.findOne({ email: user?.email });
             if (isUserExist?._id) {
                 return res.send({
@@ -81,7 +82,7 @@ async function run() {
             return res.send({ token });
         });
 
-        app.patch("/user/:email", verifyToken, async (req, res) => {
+        app.patch("/user/:email", async (req, res) => {
             const email = req.params.email;
             const userData = req.body;
             const result = await userCollection.updateOne(
@@ -89,6 +90,42 @@ async function run() {
                 { $set: userData },
                 { upsert: true }
             );
+            res.send(result);
+        });
+
+        // product
+
+        app.get("/products", async (req, res) => {
+            const result = await productCollection.find().toArray();
+            res.send(result);
+        });
+
+        app.get("/product/:id", async (req, res) => {
+            const id = req.params.id;
+            const productData = await productCollection.findOne({
+                _id: new ObjectId(id),
+            });
+            res.send(productData);
+        });
+
+        app.post("/product", async (req, res) => {
+            const productData = req.body;
+            const result = await productCollection.insertOne(productData);
+            res.send(result);
+        });
+
+        app.patch("/product/:id", async (req, res) => {
+            const id = req.params.id;
+            const updatedData = req.body;
+            const result = await productCollection.updateOne(
+                { _id: new ObjectId(id) },
+                { $set: updatedData }
+            );
+            res.send(result);
+        });
+        app.delete("/product/:id", async (req, res) => {
+            const id = req.params.id;
+            const result = await productCollection.deleteOne({ _id: new ObjectId(id) });
             res.send(result);
         });
 
